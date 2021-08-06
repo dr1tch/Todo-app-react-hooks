127b2eee0a9438ad682aafa821f3905fc25a862a
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2021-08-06 21:10:43 +0100

    building the skelton and defining routes.

---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+export const HOME = "/";
+export const LOGIN = "/login";
+export const REGISTER = "/register";
+export const LOGOUT = "/logout";
+export const PROFILE = "/me/:username";
+export const TASKS = "/tasks/all";
+export const COMPLETED = "/tasks/completed";
+export const PENDING = "/tasks/pending";
